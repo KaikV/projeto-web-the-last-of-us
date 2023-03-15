d68651fbef2afdc30a7a555940cce374498b7148
Kaik <119710298+KaikV@users.noreply.github.com>
2023-03-15 19:10:39 -0400

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem')
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        
+        selecionarBotao();
+
+        selecionarBotaoCarrossel(botao);
+         
+        esconderImagemAtiva();
+        
+        mostrarImagemDeFundo(indice);
+
+    })
+})
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function selecionarBotao() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
